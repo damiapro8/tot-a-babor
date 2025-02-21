2921640c343ff5699a16f1354090cc01f4d8bd32
Damia Turu <1666458@uab.cat>
2025-02-21 12:57:32 +0100

    millor moviment i text debug

--- funcio.js
+++ funcio.js
@@ -17,29 +17,30 @@ var config = {
     }
 };
 
-var player;
+var jugador;
 var cursors;
-var jumpSpeed = -800;  // Velocitat del salt
-var moveSpeed = 200;   // Velocitat de moviment
-var friction = 0.99;   // Factor de fricció per a l'efecte de relliscament
-var clickStartTime = 0;  // Temps de l'inici del clic
-var maxForce = 1000;     // Força màxima a aplicar
-var minForce = 200;      // Força mínima a aplicar
+
+var velMoviment = 5;   // Velocitat de moviment
+var velMaxMoviment = 300; // Velocitat maxima per moures amb A i D (no limita lo altre)
+var friccio = 0.99;   // Factor de fricció per a l'efecte de relliscament
+var ForçaMax = 800;     // Força màxima a aplicar
+var ForçaMin = 200;      // Força mínima a aplicar
 var isClicking = false;  // Si s'està mantenint el clic
-var clickDuration = 0;   // Temps de durada del clic
-var maxClickTime = 1000; // Temps màxim per mantenir el clic (en mil·lisegons)
-var VelForçaClick = 0.6;
-var ClickCooldown = 5000;
-var tempsEntreCooldown = 0;
-var tempsIniciCooldown = 0;
+var TempsFentClick = 0;   // Temps de durada del clic
+var VelForçaClick = ForçaMax/ForçaMin/10; // mutiplicador de força per temps clickant (en 2s (ForçaMax/velForçaClick) s'arriba a max)
+var ClickCooldown = 3000; // temps per poder tornar a mantenir
+var tempsEntreCooldown = 5000;
 var estaEnAigua = false;
 var jugadorPotMoures = true;
 var potFerHabilitat = false;
+
+var textDebugar;
+
 var aigua;
 var terra;
 
 function preload() {
-    this.load.image('player', './imatges/vaixell1.png');  // Carrega la imatge del personatge
+    this.load.image('jugador', './imatges/vaixell1.png');  // Carrega la imatge del personatge
     this.load.image('terra', './imatges/sorreta.png');  // Imatge del terra
     this.load.image('aigua', './imatges/aguita.png'); // imatge de l'aigua
 }
@@ -48,12 +49,12 @@ function create() {
     // Crea el jugador
     let gameWidth = this.scale.width;
     let gameHeight = this.scale.height;
-    player = this.physics.add.sprite(gameWidth / 2, gameHeight / 2, 'player');
-    player.setDepth(1);
-    // player.setCollideWorldBounds(true);  // Evita que el jugador surti de la pantalla
-    player.setDrag(10, 0);  // Aplica fricció al moviment horitzontal per un efecte de relliscament
-    player.setMaxVelocity(1000, 1000);
-    this.cameras.main.startFollow(player, false, 0.1, 0.1);
+    jugador = this.physics.add.sprite(gameWidth / 2, gameHeight / 2, 'jugador');
+    jugador.setDepth(1);
+    // jugador.setCollideWorldBounds(true);  // Evita que el jugador surti de la pantalla
+    jugador.setDrag(10, 0);  // Aplica fricció al moviment horitzontal per un efecte de relliscament
+    jugador.setMaxVelocity(1000, 1000);
+    this.cameras.main.startFollow(jugador, false, 0.1, 0.1, 0, 50);
     // Configura les tecles de moviment
     cursors = this.input.keyboard.addKeys({
         left: Phaser.Input.Keyboard.KeyCodes.A,
@@ -61,12 +62,28 @@ function create() {
         up: Phaser.Input.Keyboard.KeyCodes.SPACE
     });
 
+
+    textDebugar = this.add.text(20, 20, 'Debug Info', {
+        fontSize: '20px',
+        fill: '#ffffff',
+        fontFamily: 'Arial',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        padding: { x: 10, y: 5 }
+    }).setScrollFactor(0);
+
+    textDebugar.setDepth(100); // Assegura que el text estigui al davant de tot
+    textDebugar.setVisible(false); // Oculta el text en iniciar
+    debugKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P); // `'` en teclats espanyols
+
+
+    
+
     // Quan es fa clic, guardar el temps d'inici del clic
     this.input.on('pointerdown', (pointer) => {
         if (pointer.leftButtonDown()) { 
             if (potFerHabilitat) {
                 isClicking = true;
-                clickStartTime = this.time.now;  // Inici del temps de clic
+                TempsFentClick = 0;
             }
         }
     });
@@ -76,9 +93,9 @@ function create() {
         if (pointer.leftButtonReleased()) {
             if (potFerHabilitat) {
                 isClicking = false;  // Reinicia el clic
-                let força = Math.min(Math.max(clickDuration * VelForçaClick, minForce), maxForce);  
-                aplicaForça(calculavecJugadorRatoli(), força);
-                clickDuration = 0; // Reset del temps de clic
+                let força = Math.min(Math.max(TempsFentClick * VelForçaClick, ForçaMin), ForçaMax);  
+                aplicaForça(calculavecJugadorRatoli(this), força);
+                TempsFentClick = 0; // Reset del temps de clic
                 potFerHabilitat = false;
                 tempsEntreCooldown = 0;
             }
@@ -98,10 +115,10 @@ function create() {
         .setDisplaySize(gameWidth / 2, 50).setOrigin(0.5, 0).refreshBody();
 
     // Fer que el jugador col·lideixi amb el terra
-    this.physics.add.collider(player, terra);
+    this.physics.add.collider(jugador, terra);
 
     // Detectar quan el jugador toca l’aigua
-    this.physics.add.overlap(player, aigua, dinsAigua, null, this);
+    this.physics.add.overlap(jugador, aigua, dinsAigua, null, this);
 }
 
 function dinsAigua() {
@@ -123,19 +140,20 @@ function normalitzarVector(vector) {
     // Normalitzar el vector dividint cada component per la norma
     return [vector[0] / norma, vector[1] / norma];
 }
-function calculavecJugadorRatoli()
+
+function calculavecJugadorRatoli(scene)
 {
-    let mouseX = game.input.activePointer.x;
-    let mouseY = game.input.activePointer.y;
-    let deltaX = mouseX - player.x;
-    let deltaY = mouseY - player.y;
+    let mouseX = scene.input.activePointer.worldX; // Coordenada X real del ratolí en el món del joc
+    let mouseY = scene.input.activePointer.worldY; // Coordenada Y real del ratolí en el món del joc
+    let deltaX = mouseX - jugador.x;
+    let deltaY = mouseY - jugador.y;
     return [deltaX, deltaY];
 }
 
 function aplicaForça(vector, force) {
     let vectorNorm = normalitzarVector(vector);
-    player.setVelocityX(player.body.velocity.x + vectorNorm[0] * force);
-    player.setVelocityY(player.body.velocity.y + vectorNorm[1] * force);
+    jugador.setVelocityX(jugador.body.velocity.x + vectorNorm[0] * force);
+    jugador.setVelocityY(jugador.body.velocity.y + vectorNorm[1] * force);
 }
 
 function comprovaReduirCooldown(delta)
@@ -154,31 +172,46 @@ function comprovaReduirCooldown(delta)
 function update(time, delta) {
     // Si estem mantenint el clic, actualitza la durada del clic
     if (isClicking) {
-        clickDuration += delta;
+        TempsFentClick += delta;
     }
     comprovaReduirCooldown(delta);
 
-    estaEnAigua = this.physics.overlap(player, aigua);
+    estaEnAigua = this.physics.overlap(jugador, aigua);
     jugadorPotMoures = estaEnAigua;
 
     // Moviment cap a l'esquerra i dreta amb un efecte de relliscament
     if (cursors.left.isDown && jugadorPotMoures) {
-        player.setVelocityX(-moveSpeed);  // Moviment cap a l'esquerra
+        if (jugador.body.velocity.x > -velMaxMoviment) { 
+            aplicaForça([-1, 0], velMoviment);
+        }
     } else if (cursors.right.isDown && jugadorPotMoures) {
-        player.setVelocityX(moveSpeed);  // Moviment cap a la dreta
-    } else if (player.body.touching.down) {
+        if (jugador.body.velocity.x < velMaxMoviment) { 
+            aplicaForça([1, 0], velMoviment);
+        }
+    } else if (jugador.body.touching.down) {
         // Si el jugador està a terra i no es prem cap tecla, aplica més fricció
-        player.setVelocityX(player.body.velocity.x * friction); // Fricció només quan estigui a terra
+        jugador.setVelocityX(jugador.body.velocity.x * friccio); // Fricció només quan estigui a terra
     }
 
     // Quan estigui en l'aire (no toqui el terra), no s'aplica fricció
-    if (!player.body.touching.down) {
+    if (!jugador.body.touching.down) {
         // Desactivar fricció en l'aire
-        player.setDrag(20, 0);  // Desactivar fricció horitzontal en l'aire
+        jugador.setDrag(20, 0);  // Desactivar fricció horitzontal en l'aire
     } else {
         // Aplicar fricció només quan estigui a terra
-        player.setDrag(100, 0); // Torna a aplicar fricció a terra
+        jugador.setDrag(100, 0); // Torna a aplicar fricció a terra
     }
+
+    if (Phaser.Input.Keyboard.JustDown(debugKey)) {
+        textDebugar.setVisible(!textDebugar.visible); // Alterna la visibilitat
+    }
+    textDebugar.setText(
+        `VelX: ${jugador.body.velocity.x.toFixed(2)}\n` +
+        `VelY: ${jugador.body.velocity.y.toFixed(2)}\n` +
+        `Temps Click: ${TempsFentClick.toFixed(2)} ms\n` +
+        `Pot fer habilitat: ${potFerHabilitat} \n` +
+        `Cooldown: ${tempsEntreCooldown}`
+    );
 }
 
 // Inicialitza el joc
