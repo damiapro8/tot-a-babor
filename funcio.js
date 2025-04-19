1b432e77400fbf27a96d9137e35df4626310f1a4
Damià Turu Pérez <77452844+damiapro8@users.noreply.github.com>
2025-04-19 15:09:06 +0200

    multijugador hotfix

--- funcio.js
+++ funcio.js
@@ -195,11 +195,13 @@ function create() {
         for (let id in jugadors) {
             if (id !== socket.id) {
                 if (!altresJugadors[id]) {
-                    let sprite = game.scene.scenes[0].add.sprite(jugadors[id].x, jugadors[id].y, 'jugador');
+                    let sprite = game.scene.scenes[0].physics.add.sprite(jugadors[id].x, jugadors[id].y, 'jugador');
                     player.setTintFill(0x88ccff); // blau fluix
                     player.setAlpha(0.6); // una mica transparent
                     sprite.setDepth(0);
                     altresJugadors[id] = sprite;
+                    sprite.body.setAllowGravity(false);
+                    sprite.body.moves = false; // no volem que la física el mogui gens
                 } else {
                     altresJugadors[id].setPosition(jugadors[id].x, jugadors[id].y);
                 }
